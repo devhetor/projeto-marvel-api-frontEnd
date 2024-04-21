17877e6e32ba0099819000c57ac65be3f4853560
heitor <heitorbrandolin@hotmail.com>
2024-04-21 17:40:45 -0300

    Show all Creators

--- comic-library/src/components/Search.jsx
+++ comic-library/src/components/Search.jsx
@@ -2,10 +2,12 @@ import { useState } from "react";
 import "../styles/Search.scss";
 import Characters from "./Characters";
 import Comics from "./Comics";
+import Creators from "./Creators";
 
 export default function Search() {
   const [characterData, setCharacterData] = useState(null);
   const [comicData, setComicData] = useState(null);
+  const [creatorsData, setCreatorsData] = useState(null);
   const [characterName, setCharacterName] = useState("");
 
   const handleSubmit = (event) => {
@@ -39,12 +41,31 @@ export default function Search() {
       .then((response) => response.json())
       .then((result) => {
         setComicData(result);
-        console.log(result);
       })
       .catch(() => {
         console.log("error while getting character data");
       });
   };
+
+  const getAllCreatorsData = () => {
+    setCharacterData(null);
+    setComicData(null);
+    setCreatorsData(null);
+
+
+    const url = 'http://localhost:3000/creators';
+
+    fetch(url)
+      .then((response) => response.json())
+      .then((result) => {
+        setComicData(result);
+      })
+      .catch(() => {
+        console.log("error while getting character data");
+      });
+  };
+
+  //TODO
 
   // const getComicData = (characterId) => {
   //   window.scrollTo({ top: 0, left: 0 });
@@ -69,10 +90,13 @@ export default function Search() {
     setCharacterName("");
     setCharacterData(null);
     setComicData(null);
+    setCreatorsData(null);
   };
 
   return (
     <>
+
+      <img className="img" src="https://static.wikia.nocookie.net/logocomics/images/d/d2/1602%282003-2004%29.png" alt="logo" />
       <form className="search" onSubmit={handleSubmit}>
         <input
           type="text"
@@ -80,14 +104,15 @@ export default function Search() {
           onChange={handleChange}
         />
         <div className="buttons">
-          <button type="submit">Get Characters</button>
-          <button type="submit" onClick={getAllComicData}>Get Comics</button>
+          <button type="submit">Characters</button>
+          <button type="submit" onClick={getAllComicData}>Comics</button>
+          <button type="submit" onClick={getAllCreatorsData}>Creators</button>
           <button type="reset" className="reset" onClick={handleReset}>
             Reset
           </button>
         </div>
       </form>
-      
+
 
       {!comicData && characterData && characterData[0] && (
         <Characters data={characterData} onClick={getAllComicData} /> //TODO
@@ -95,6 +120,10 @@ export default function Search() {
 
       {comicData && comicData[0] && (
         <Comics data={comicData} onClick={getAllComicData} />
+      )}
+
+      {creatorsData && creatorsData[0] && (
+        <Creators data={creatorsData} onClick={getAllCreatorsData} />
       )}
     </>
   );
